a7bc2c0f089ddad15789121b41c62e64116c9662
Patrick Zhang <patdujour@gmail.com>
2016-09-28 17:10:31 -0700

    - Rewrite angular directives and controller

--- js/app.js
+++ js/app.js
@@ -1,77 +1,75 @@
 (function() {
   'use strict';
   var angular  = require('angular');
-  angular.module('iQuit', [require('angular-animate'),
+  var app = angular.module('iQuit', [require('angular-animate'),
                            require('angular-formly'),
-                           require('angular-formly-templates-bootstrap'),])
-  .controller('mainCtrl', mainCtrl);
+                           require('angular-formly-templates-bootstrap'),],
+                 function config(formlyConfigProvider){
+                    // set templates here
+                    formlyConfigProvider.setType({
+                      name: 'custom',
+                      templateUrl: 'custom.html'
+                    });
+                  });
 
-  function mainCtrl() {
-    var vm = this;
-    vm.obSubmit = onSubmit;
-    // The model object that we reference
-    // on the  element in index.html
-    vm.model = {};
+  app.controller('mainCtrl', ["$scope", function($scope) {
+    $scope.user = {}; // the model 'user'
 
-    // An array of our form fields with configuration
-    // and options set. We make reference to this in
-    // the 'fields' attribute on the  element
-    vm.fields = [
-        {
-            key: 'name',
-            type: 'input',
-            templateOptions: {
-                type: 'text',
-                label: 'Your Name',
-                placeholder: 'Michael Kang',
+  	$scope.userFields = [
+    {
+      key: 'name',
+      type: 'input',
+      templateOptions: {
+        type: 'text',
+        label: 'Your Name',
+        placeholder: 'Michael Kang',
+      }
+    },
+    {
+      key: 'boss',
+      type: 'input',
+      templateOptions: {
+        type: 'text',
+        label: 'Boss',
+        placeholder: 'John Doe',
+      }
+    },
+    {
+      key: 'company',
+      type: 'input',
+      templateOptions: {
+        type: 'text',
+        label: 'Company',
+        placeholder: 'THE COMPANY',
+      }
+    },
+    {
+      key: 'time',
+      type: 'input',
+      templateOptions: {
+        type: 'text',
+        label: 'Time at Company',
+        placeholder: '4 YEARS 3 MONTHS',
+      }
+    },
+    {
+      key: 'Reason',
+      type: 'textarea',
+      templateOptions: {
+        type: 'text',
+        label: 'Reason',
+        placeholder: "I DON'T CARE ANY MORE.",
+      }
+    },
+ 	]; // fields/keys we need in the model as a list
 
-            }
-        },
-        {
-            key: 'boss',
-            type: 'input',
-            templateOptions: {
-                type: 'text',
-                label: 'Boss',
-                placeholder: 'John Doe',
 
-            }
-        },
-        {
-            key: 'company',
-            type: 'input',
-            templateOptions: {
-                type: 'text',
-                label: 'Company',
-                placeholder: 'THE COMPANY',
-
-            }
-        },
-        {
-            key: 'time',
-            type: 'input',
-            templateOptions: {
-                type: 'text',
-                label: 'Time at Company',
-                placeholder: '4 YEARS 3 MONTHS',
-
-            }
-        },
-        {
-            key: 'Reason',
-            type: 'textarea',
-            templateOptions: {
-                type: 'text',
-                label: 'Reason',
-                placeholder: "I DON'T CARE ANY MORE.",
-
-            }
-        },
-    ];
-
-    function onSubmit() {
-      console.log('called');
-      alert(JSON.stringify(vm.model), null, 2);
+    // function definition
+    function onSubmit(user) {
+      alert(JSON.stringify(user));
     }
-  }
+    $scope.onSubmit = onSubmit;
+
+  }]);
+
 })();
